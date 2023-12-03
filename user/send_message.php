c40df381f57d3d0f361f4c9e32b1bb382d163e7d
2120865 <132554578+2120865@users.noreply.github.com>
2023-12-03 19:29:04 +0100

    Update send_message.php

--- user/send_message.php
+++ user/send_message.php
@@ -1,9 +1,14 @@
 <?php
 	session_start();
 	include('../conn.php');
+	date_default_timezone_set('Europe/Berlin');
 	if(isset($_POST['msg'])){		
 		$msg=$_POST['msg'];
 		$id=$_POST['id'];
-		mysqli_query($conn,"insert into `chat` (chatroomid, message, userid, chat_date) values ('$id', '$msg' , '".$_SESSION['id']."', NOW())") or die(mysqli_error($conn));
+		$currentDateTime = date('Y-m-d H:i:s');
+
+		$query = "INSERT INTO `chat` (chatroomid, message, userid, chat_date) VALUES ('$id', '$msg', '" . $_SESSION['id'] . "', '$currentDateTime')";
+
+		mysqli_query($conn, $query) or die(mysqli_error());
 	}
 ?>
